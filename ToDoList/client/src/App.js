43c1c8ad6e780b3d275d7744f43a6950951178c7
ox01220122 <ox0122@gmail.com>
2023-06-28 09:55:15 +0900

    css 설정

--- ToDoList/client/src/App.js
+++ ToDoList/client/src/App.js
@@ -1,13 +1,10 @@
-import "./App.css";
 import TodoTitle from "./components/TodoTitle";
 import Render from "./components/Render";
-import AddData from "./components/AddData";
 
 function App() {
   return (
     <>
       <TodoTitle></TodoTitle>
-      <AddData></AddData>
       <Render></Render>
     </>
   );
--- ToDoList/client/src/components/Render.js
+++ ToDoList/client/src/components/Render.js
@@ -1,6 +1,8 @@
 import "./componentsCss/Render.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 const TodoTitle = (props) => {
   const [todoData, setTodoData] = useState([]);
@@ -10,18 +12,47 @@ const TodoTitle = (props) => {
       .then((response) => setTodoData(response.data));
   }, []);
 
+  function AddData() {
+    useEffect(() => {
+      axios
+        .get("http://localhost:8000/todos")
+        .then((response) => setTodoData(response.data));
+    }, []);
+  }
+
   return (
     <>
-      {todoData.map((todo) => (
-        <div key={todo.id} className="show-todo">
-          {todo.done === true ? (
-            <input type="checkbox" checked="true"></input>
-          ) : (
-            <input type="checkbox"></input>
-          )}
-          <div>{todo.title}</div>
+      <div className="input-data">
+        <input
+          type="text"
+          placeholder="Add your new Todo"
+          className="input-data-text"
+        ></input>
+        <button className="add-button">ADD</button>
+      </div>
+      {todoData.length === 0 ? (
+        <div>
+          <div className="null-msg">ToDoList를 추가하세요 ! </div>
         </div>
-      ))}
+      ) : (
+        todoData.map((todo) => (
+          <div key={todo.id} className="show-todo">
+            <span className="check-list">
+              {todo.done === true ? (
+                <input type="checkbox" checked={true} />
+              ) : (
+                <input type="checkbox" />
+              )}
+              <div>
+                <div className="todo-title">{todo.title}</div>
+              </div>
+            </span>
+            <button className="trash-button">
+              <FontAwesomeIcon icon={faTrashCan} />
+            </button>
+          </div>
+        ))
+      )}
     </>
   );
 };
